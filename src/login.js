0b01b578083d2502411d749da4a0d83a5c50bc4a
#87 <hasheightseven@gmail.com>
2024-10-25 02:19:24 +0530

    Update login.js

--- src/login.js
+++ src/login.js
@@ -1,1 +1,7 @@
-import { mediascreenstyle } from "./index.js";
+const loginform = document.createElement("form");
+loginform.style.margin = 0;
+loginform.style.display = "flex";
+loginform.style.alignItems = "center";
+loginform.style.justifyContent = "center";
+loginform.style.padding = 0;
+document.body.appendChild(loginform);
