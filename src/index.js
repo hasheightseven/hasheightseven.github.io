cc1b50154d996c90d9d05559553d07d251fadc38
#87 <hasheightseven@gmail.com>
2024-10-24 22:41:21 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const h87 = document.body;
 h87.style.backgroundColor = "#002611";
 h87.appendChild(mediascreenstyle);
 
-const h87h1 = document.createElement("h1");
+const h87h1 = document.createElement("div");
 h87h1.textContent = "#87";
 h87h1.style.fontFamily = "Helvetica Neue, Helvetica Sans, sans-serif";
 h87h1.style.fontSize = "420%";
