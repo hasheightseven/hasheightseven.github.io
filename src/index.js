e40ffcb062649fd860d6928381ab283a2d25cb3c
#87 <hasheightseven@gmail.com>
2024-10-24 23:48:48 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -57,12 +57,12 @@ h87login.addEventListener("click", function () {
 });
 h87login.addEventListener("mouseover", function () {
   h87login.style.cursor = "pointer";
-  h87login.style.textShadow = "0 0 0.87em #002611";
+  h87login.style.textShadow = "0 0 0.87em #959200";
   h87login.style.boxShadow = "0 0 0.87em 0 #959200";
 });
 h87login.addEventListener("mouseout", function () {
   h87login.style.textShadow = "0 0 0.87em #959200";
-  h87login.style.boxShadow = "0 0 0.87em 0 #002611";
+  h87login.style.boxShadow = "0 0 0.87em 0 #959200";
 });
 
 const h87signup = document.createElement("button");
@@ -89,12 +89,12 @@ h87signup.addEventListener("click", function () {
 });
 h87signup.addEventListener("mouseover", function () {
   h87signup.style.cursor = "pointer";
-  h87signup.style.textShadow = "0 0 0.11em #002611";
-  h87signup.style.boxShadow = "0 0 0.11em 0 #959200";
+  h87signup.style.textShadow = "0 0 0.87em #959200";
+  h87signup.style.boxShadow = "0 0 0.87em 0 #959200";
 });
 h87signup.addEventListener("mouseout", function () {
-  h87login.style.textShadow = "0 0 0.87em #959200";
-  h87login.style.boxShadow = "0 0 0.87em 0 #002611";
+  h87signup.style.textShadow = "0 0 0.87em #959200";
+  h87signup.style.boxShadow = "0 0 0.87em 0 #959200";
 });
 
 h87.appendChild(h87h1);
