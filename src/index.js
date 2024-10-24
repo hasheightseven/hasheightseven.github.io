2f32185ff126c5a56adabd03187240b3bced96f5
#87 <hasheightseven@gmail.com>
2024-10-24 21:45:07 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ h87h1.style.fontFamily = "Helvetica Neue, Helvetica Sans, sans-serif";
 h87h1.style.fontSize = "169%";
 h87h1.style.backgroundColor = "#290260";
 h87h1.style.color = "transparent";
-h87h1.style.textShadow = "0 0 0.26em #290260";
+h87h1.style.textShadow = "0 0 0.26em #002611";
 
 const h87div1 = document.createElement("div");
 h87div1.style.width = "100%";
@@ -30,9 +30,9 @@ h87div1.style.boxShadow = "inset 0 0 8% radial-gradient(#002611, #000429)";
 
 const h87login = document.createElement("button");
 h87login.textContent = "login";
-h87login.setAttribute("width", "100%");
-h87login.setAttribute("height", "100%");
-h87login.style.padding = 0;
+h87login.setAttribute("width", "250px");
+h87login.style.paddingLeft = "26px";
+h87login.style.fontSize = "300%";
 h87login.style.margin = 0;
 
 h87.appendChild(h87h1);
