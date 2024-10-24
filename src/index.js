aef844684c6f0a41944ebca6aa66405ea3e48c09
#87 <hasheightseven@gmail.com>
2024-10-25 00:16:23 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ mediascreenstyle.textContent = `
   box-sizing: border-box;
 }
 html, body {
-  background: linear-gradient(69deg, #002611, #959200) no-repeat cover};
+  background: linear-gradient(26deg, #002611 #959200) no-repeat cover;
+}
+
 `;
 const h87 = document.body;
 // h87.style.backgroundColor = "#002611";
