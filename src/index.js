c3adda25125970cff901dbfbd40a9da10543620e
#87 <hasheightseven@gmail.com>
2024-10-24 22:17:53 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -43,8 +43,8 @@ h87div1.appendChild(h87login);
 const canvas = document.createElement("canvas");
 h87.appendChild(canvas);
 const ctx = canvas.getContext("2d");
-canvas.setAttribute("width", "8em");
-canvas.setAttribute("height", "8em");
+canvas.setAttribute("width", "400px");
+canvas.setAttribute("height", "500px");
 // Circle properties
 let x = canvas.width / 2;
 let y = canvas.height / 2;
