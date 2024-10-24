13f1e9286fc14a434b5f15f1b6f3650446e2068f
#87 <hasheightseven@gmail.com>
2024-10-24 21:40:26 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const starttime = Date.now();
 
+const mediascreenstyle = document.createElement("style");
+mediascreenstyle.textContent = `
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+`;
 const h87 = document.body;
 h87.style.backgroundColor = "#002611";
 
@@ -9,6 +17,7 @@ h87h1.style.fontFamily = "Helvetica Neue, Helvetica Sans, sans-serif";
 h87h1.style.fontSize = "169%";
 h87h1.style.backgroundColor = "#290260";
 h87h1.style.color = "transparent";
+h87h1.style.textShadow = "0 0 0.26em #290260";
 
 const h87div1 = document.createElement("div");
 h87div1.style.width = "100%";
