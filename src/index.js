77149ad14c5545cff97b88642df0bf2936acc619
#87 <hasheightseven@gmail.com>
2024-10-25 00:25:27 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -70,7 +70,7 @@ h87login.addEventListener("mouseover", function () {
   h87login.style.boxShadow = "0 0 0.87em 0 #959200";
 });
 h87login.addEventListener("mouseout", function () {
-  h87login.style.textShadow = "none";
+  h87login.style.textShadow = "0 0 0.011em #002611";
   h87login.style.boxShadow = "inset 0 0 0.87em 0 #959200";
 });
 
@@ -104,7 +104,7 @@ h87signup.addEventListener("mouseover", function () {
   h87signup.style.boxShadow = "0 0 0.87em 0 #959200";
 });
 h87signup.addEventListener("mouseout", function () {
-  h87signup.style.textShadow = "none";
+  h87signup.style.textShadow = "0 0 0.011em #002611";
   h87signup.style.boxShadow = "inset 0 0 0.87em 0 #959200";
 });
 
