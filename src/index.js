b15a57dd661f01a4f2dd31472e15d5f8c05da265
#87 <hasheightseven@gmail.com>
2024-10-24 21:08:51 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -19,8 +19,15 @@ h87.style.backgroundColor = "var(--h87-color)";
 const h87h1 = document.createElement("h1");
 const h87div1 = document.createElement("div");
 
+const h87login = document.createElement("button");
+h87login.setAttribute("width", "100%");
+h87login.setAttribute("height", "100%");
+h87login.style.padding = 0;
+h87login.style.margin = 0;
+
 h87.addEventListener("load", function (event) {
   event.preventDefault();
   // the whole html goes here
   h87.appendChild(h87h1 + h87div1);
+  h87div1.appendChild(h87login);
 });
