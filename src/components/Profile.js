b7338d46c3698cd08234ab4b0bd6f298033c97da
#87 <hasheightseven@gmail.com>
2024-10-24 15:46:17 +0530

    changes

--- src/components/Profile.js
+++
@@ -1,8 +0,0 @@
-export default function Profile() {
-    return(
-        <>
-            <h2>Profile Name</h2>
-            <img src="https://i.imgur.comMK3eW3Am.jpg" alt="Katherine Johnson" />
-        </>
-    )
-}
