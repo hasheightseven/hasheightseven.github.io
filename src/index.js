5d27b5e4ab1fb2a79448b0497ef6b7eeb2acb51b
#87 <hasheightseven@gmail.com>
2024-10-24 15:38:01 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // REF: https://react.dev/learn/add-react-to-an-existing-project
-import { createRoot } from "../react-dom/client";
+import { createRoot } from "/node_modules/react-dom/client";
 
 // clear the existing HTML content
 document.body.innerHTML = `<div id="hashtag-87"></div>`;
