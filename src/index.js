4c7acc382aeddbd11d29c11c96565a34ecc3b0e5
#87 <hasheightseven@gmail.com>
2024-10-25 11:00:14 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -144,6 +144,8 @@ copyrightimg.setAttribute(
 copyrightimg.setAttribute("alt", " © ");
 
 const h87copy = document.createElement("div");
+h87copy.style.fontSize = "300%";
+h87copy.style.padding = "0.26em auto";
 h87copy.textContent = `#87 © ${copyrightimg.innerHTML} 2024`;
 
 h87.appendChild(h87h1);
