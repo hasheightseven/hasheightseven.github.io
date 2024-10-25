3b66a0e56409b89c7ea940e7c7f13019a08dc6b3
#87 <hasheightseven@gmail.com>
2024-10-25 13:16:42 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+// A simple Button component
+const Button = ({ label, onClick }) => {
+  return <button onClick={onClick}>{label}</button>;
+};
+
+// Parent component using the Button
+const App = () => {
+  const handleClick = () => {
+    alert("Button clicked!");
+  };
+
+  return (
+    <div>
+      <h1>Component-Based Programming</h1>
+      <Button label="Click Me" onClick={handleClick} />
+    </div>
+  );
+};
+
+export default App;
