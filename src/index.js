1ec0f4271409e0806a761ce7b288e638fbfbac67
#87 <hasheightseven@gmail.com>
2024-10-25 16:12:52 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import Routes from "./Routes";
 import { Provider } from "react-redux";
 import store from "./store";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <Routes />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
