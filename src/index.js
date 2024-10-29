7cbfbe2443c7e9b44a5bae8149ebe75c810fe15a
#87 <hasheightseven@gmail.com>
2024-10-30 01:29:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
-import React from "react";
-import ReactDOM from "/react-dom/client";
-import "./index.css";
-import App from "./App";
-import Routes from "./Routes";
-import { Provider } from "react-redux";
-import store from "./store";
-
-const rootelement = document.createElement("div");
-rootelement.setAttribute("id", "root");
-document.body.appendChild(rootelement);
-const root = ReactDOM.createRoot(rootelement);
-
-root.render(`
-  <React.StrictMode>
-    <Provider store=${store}>
-      <Routes />
-    </Provider>
-  </React.StrictMode>
-  `);
+document.body.addEventListener("load", () => {
+    document.body.style.background = "radial-gradient(corcle, #261192, #042995, #261192, #042995, #261192, #042995, #261192, #042995)";
+});
