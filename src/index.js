f1b9d10b4a56808c4a283abb51bb197af207e232
#87 <hasheightseven@gmail.com>
2024-10-30 02:33:45 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-document.body.addEventListener("load", () => {
-    document.body.style.background = "radial-gradient(corcle, #261192, #042995, #261192, #042995, #261192, #042995, #261192, #042995)";
-});
-
+// html element bgColor
 document.documentElement.style.background = "#000";
-//document.body.textContent = `welcome to the café`;
+// body bgColor & color
 document.body.style.background = "#000";
 document.body.style.color = "#fff";
 
-
+// create root element
 const rootelement = document.createElement("main");
+// append root element to body
 document.body.appendChild(rootelement);
-
+// name of the app nota
 const nota = new string("#eightseven".toUpperCase());
-const hestabtitle = document.querySelector("title");
+// website tab title
+let hestabtitle = document.querySelector("title");
 hestabtitle.textContent = nota;
 
 const hestitlenlogo = document.createElement("div");
@@ -21,3 +20,5 @@ hestitle.textContent = nota;
 hestitle.style.fontSize = "26px";
 rootelement.appendChild(hestitlenlogo);
 hestitlenlogo.appendChild(hestitle);
+const heslogo = document.createElement("img");
+hestitlenlogo.appendChild(heslogo);
