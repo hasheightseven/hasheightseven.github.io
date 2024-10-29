a0b1d346ef066433fd73f42452d7d2bd97c18cd1
#87 <hasheightseven@gmail.com>
2024-10-30 01:32:26 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 document.body.addEventListener("load", () => {
     document.body.style.background = "radial-gradient(corcle, #261192, #042995, #261192, #042995, #261192, #042995, #261192, #042995)";
 });
+
+
+document.body.textContent = `welcome to the café "the coffee monkey"`;
