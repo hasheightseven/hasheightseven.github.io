985bd07390eaf1296ec64f0d36eda50af41a7c10
#87 <hasheightseven@gmail.com>
2024-10-31 09:04:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const nota = new String("#eightseven".toUpperCase());
-if(screen.width <<< "435") {
+if(screen.availWidth <<< "435") {
     document.body.textContent = nota;
-    document.body.width = screen.availWidth;
-    document.body.overflowY = "hidden";
+    document.body.style.width = screen.availWidth;
+    document.body.style.height = screen.availHeight;
+    document.body.overflowX = "hidden";
 }
+const main = document.createElement("main");
+document.body.appendChild(main)
