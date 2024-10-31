fa0708d16d60ac6dc6a30a1f46a8c40697f6c9eb
#EightSeven <hasheightseven@gmail.com>
2024-10-31 20:58:16 +0530

    Delete src/login.js

--- src/login.js
+++
@@ -1,7 +0,0 @@
-const loginform = document.createElement("form");
-loginform.style.margin = 0;
-loginform.style.display = "flex";
-loginform.style.alignItems = "center";
-loginform.style.justifyContent = "center";
-loginform.style.padding = 0;
-document.body.appendChild(loginform);
