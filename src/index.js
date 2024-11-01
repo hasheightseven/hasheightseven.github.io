33854aa30c1612b845a39960cfb33b85d3adf7c3
#EightSeven <hasheightseven@gmail.com>
2024-11-01 21:57:24 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -9,33 +9,20 @@ document.body.style.background = bgImage;
 const main = document.createElement("main");
 document.body.appendChild(main);
 
-//main.addEventListener("load", () => {
-    //main.setAttribute("width", screen.availWidth);
-    //main.setAttribute("height", screen.availHeight);
-    //main.style.backgroundColor = "#112";
-    //main.style.color = "#042995";
-    
-    //main.setAttribute("background-color") = "rgba(126,126,126,0.5)";
-    
-//});
-
 const logoimg = document.createElement("div");
-//ogoimg.appendChild(fs.read("/assets/media/Bg-Img-HES.png"));
-//main.appendChild(logoimg);
-
-
 
 function getRandomEmoji() {
   const emojis = ["🍕", "🌮", "🍣", "🍩", "🍒", "🌈", "🚀", "🌎", "🎉", "💥"];
   return emojis[Math.floor(Math.random() * emojis.length)];
 }
-  const emojiText = document.createElement("h1");
-  emojiText.textContent = nota;
+const emojiText = document.createElement("h1");
+emojiText.textContent = nota;
 main.appendChild(emojiText);
+
 function setRandomEmojiBackground() {
   const randomEmoji = getRandomEmoji();
   emojiText.style.backgroundImage = `repeating-linear-gradient(45deg, ${randomEmoji} 0, ${randomEmoji} 20%)`;
 }
 
 // Set a random emoji background on load
-window.onload = setRandomEmojiBackground;
+window.onload = setRandomEmojiBackground();
