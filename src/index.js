8343bbea4db3c261f9997fb1d752a3e5188c32b7
#EightSeven <hasheightseven@gmail.com>
2024-11-01 19:17:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const nota = new String("#eightseven".toUpperCase());
 const tabtitle = document.querySelector('title');
 tabtitle.textContent = "#EightSSeven";
 tabtitle.style.letterSpacing = '2.6px';
-const bgImage = "/assets/media/Bg-Img-HES.png";
+const bgImage = "/assets/media/Bg-Img-HES.PNG";
 document.body.style.background = "url(bgImage)";
 
 const main = document.createElement("main");
