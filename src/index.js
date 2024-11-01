5e7471b40b3a06a9646d5ed5e6cde96cf01b65c3
#EightSeven <hasheightseven@gmail.com>
2024-11-01 20:17:16 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const nota = new String("#eightseven".toUpperCase());
 const tabtitle = document.querySelector('title');
 tabtitle.textContent = "#EightSSeven";
 tabtitle.style.fontFamily = "Chokokutai";
-const bgImage = "/assets/media/Bg-Img-HES.PNG";
+const bgImage = new Image("/assets/media/Bg-Img-HES.PNG");
 document.body.style.background = "url(bgImage)";
 
 const main = document.createElement("main");
