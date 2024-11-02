3c072abd9be39ada16ce8634d1c22b64d2b066ba
#EightSeven <hasheightseven@gmail.com>
2024-11-02 17:14:07 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -17,31 +17,6 @@ main.style.alignItems = 'center';
 main.style.justifyContent = 'center';
 
 document.body.appendChild(main);
-const logoimg = document.createElement("div");
-logoimg.innerHTML = bgImage;
-//main.appendChild(logoimg);
-
-const emojiText = document.createElement("h1");
-emojiText.textContent = nota;
-emojiText.style.fontFamily = "Chokokutai, Arial, Sans-Serif System-UI";
-emojiText.style.color = '#001928';
-emojiText.style.colorScheme = 'revert-layer';
-emojiText.style.alignSelf = 'flex-end';
-
-main.appendChild(emojiText);
-
-function getRandomEmoji() {
-  const emojis = ["🍕", "🌮", "🍣", "🍩", "🍒", "🌈", "🚀", "🌎", "🎉", "💥"];
-  return emojis[Math.floor(Math.random() * emojis.length)];
-}
-
-function setRandomEmojiBackground() {
-  const randomEmoji = getRandomEmoji();
-  emojiText.style.backgroundImage = `repeating-linear-gradient(45deg, ${randomEmoji} 0, ${randomEmoji} 20%)`;
-}
-
-// Set a random emoji background on load
-setRandomEmojiBackground();
 
 const copyrightelem = document.createElement('meta');
 copyrightelem.setAttribute('name', 'copyright');
