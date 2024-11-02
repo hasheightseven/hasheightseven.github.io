548e930798cd0cba61caf6b74efacb673600e744
#EightSeven <hasheightseven@gmail.com>
2024-11-02 22:06:29 +0530

    Update forms.js

--- src/forms.js
+++ src/forms.js
@@ -2,4 +2,12 @@ const loginform = document.createElement('form');
 const signupform = document.createElement('form');
 const contactform = document.createElement('form');
 
+const label = document.createElement("h3");
+label.style.fontFamily = "chokokutai";
+label.style.color = "#112692";
+
+for (i in [loginform,signupform,contactform]){
+    i.appendChild(label);
+    }
+
 export { loginform, signupform, contactform }
