7c5ebe7ee975b169461a2725e7e05e9dc7e9e7f8
#EightSeven <hasheightseven@gmail.com>
2024-11-02 15:30:01 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-import defaultstyles from './styles.js';
-import main from './main.js';
+
+document.body.addEventListener("load", () => {
+
+  import defaultstyles from './styles.js';
+  import main from './main.js';
+});
