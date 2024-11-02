971e8c4f5de1ebcc06db1f6458d1dab4722ceb5e
#EightSeven <hasheightseven@gmail.com>
2024-11-02 23:38:33 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import defaultstyles from './styles.js';
 import main from './main.js';
 import container from "./container.js";
 main.appendChild(container);
+import { loginform, signupform, contactform } from "./forms.js";
+
+main.appendChild(loginform);
