9c4990fbfc66c8a5f5766a281df3cf1edba454d5
#EightSeven <hasheightseven@gmail.com>
2024-11-02 14:31:53 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const nota = new String("#8seven".toUpperCase());
+export const nota = new String("#8seven".toUpperCase());
 
 
-const bgImage = "url(./assets/media/heslogo.svg)";
+export const bgImage = "url(./assets/media/heslogo.svg)";
 
-const defaultstyles = document.createElement("style");
+export const defaultstyles = document.createElement("style");
 document.head.appendChild(defaultstyles);
 defaultstyles.setAttribute("type", "text/css");
 defaultstyles.setAttribute("media", "screen");
@@ -14,35 +14,12 @@ defaultstyles.innerText = `
   box-sizing: border-box;
 }`;
 
-const tabtitle = document.querySelector('title');
+export const tabtitle = document.querySelector('title');
 tabtitle.textContent = "#8Seven";
 tabtitle.style.fontFamily = "Chokokutai";
 
 // document.body.style.background = bgImage;
 
 import main from './main.js';
-document.body.appendChild(main);
-const logoimg = document.createElement("div");
-logoimg.innerHTML = bgImage;
-//main.appendChild(logoimg);
 
-const emojiText = document.createElement("h1");
-emojiText.textContent = nota;
-emojiText.style.color = '#001928';
-emojiText.style.alignSelf = 'flex-end';
-
-main.appendChild(emojiText);
-
-function getRandomEmoji() {
-  const emojis = ["🍕", "🌮", "🍣", "🍩", "🍒", "🌈", "🚀", "🌎", "🎉", "💥"];
-  return emojis[Math.floor(Math.random() * emojis.length)];
-}
-
-function setRandomEmojiBackground() {
-  const randomEmoji = getRandomEmoji();
-  emojiText.style.backgroundImage = `repeating-linear-gradient(45deg, ${randomEmoji} 0, ${randomEmoji} 20%)`;
-}
-
-// Set a random emoji background on load
-setRandomEmojiBackground();
 document.head.appendChild(`<meta name="copyright" content="Copyright &COPY; 2024 #8Seven" />`);
--- src/main.js
+++ src/main.js
@@ -10,4 +10,29 @@ main.style.display = 'flex';
 main.style.flexDirection = 'column';
 main.style.alignItems = 'center';
 main.style.justifyContent = 'center';
+
+document.body.appendChild(main);
+const logoimg = document.createElement("div");
+logoimg.innerHTML = bgImage;
+//main.appendChild(logoimg);
+
+const emojiText = document.createElement("h1");
+emojiText.textContent = nota;
+emojiText.style.color = '#001928';
+emojiText.style.alignSelf = 'flex-end';
+
+main.appendChild(emojiText);
+
+function getRandomEmoji() {
+  const emojis = ["🍕", "🌮", "🍣", "🍩", "🍒", "🌈", "🚀", "🌎", "🎉", "💥"];
+  return emojis[Math.floor(Math.random() * emojis.length)];
+}
+
+function setRandomEmojiBackground() {
+  const randomEmoji = getRandomEmoji();
+  emojiText.style.backgroundImage = `repeating-linear-gradient(45deg, ${randomEmoji} 0, ${randomEmoji} 20%)`;
+}
+
+// Set a random emoji background on load
+setRandomEmojiBackground();
 export default main;
