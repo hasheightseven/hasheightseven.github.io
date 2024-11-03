20d70b8557b1400cb8b12f3d28c5cfcd37d605c6
#EightSeven <hasheightseven@gmail.com>
2024-11-03 10:58:38 +0530

    changes

--- src/container.js
+++ src/container.js
@@ -1,6 +1,7 @@
 const container = document.createElement("div");
 container.style.width = "100%";
-container.style.width = "100%";
-container.style.backgroundColor = "#f1f1f1";
+container.style.height = "100%";
+container.style.backgroundColor = "transparent";
+
 
 export default container
