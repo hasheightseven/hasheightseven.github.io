6e116e1d776d30fd7c4eb725525adbfd2fde7fbd
#EightSeven <hasheightseven@gmail.com>
2024-11-05 15:50:47 +0530

    changes

--- src/main.js
+++ src/main.js
@@ -5,25 +5,25 @@ const bgImage = "url(./assets/media/reansparentbgeight.svg)";
 
 const main = document.createElement("main");
 //document.body.appendChild(main);
-main.style.width = '100vw';
+main.style.width = 'auto';
 main.style.height = '100vh';
 main.style.backgroundColor = "rgb(11,26,92)";
 main.style.backgroundImage = bgImage;
 main.style.backgroundRepeat = "no-repeat";
 main.style.backgroundSize = "contain";
 main.style.backgroundPosition = "center";
-main.style.display = 'flex';
-main.style.flexDirection = 'column';
-main.style.alignItems = 'center';
-main.style.justifyContent = 'center';
+// main.style.display = 'flex';
+// main.style.flexDirection = 'column';
+// main.style.alignItems = 'center';
+// main.style.justifyContent = 'center';
 
 document.body.appendChild(main);
 
 
 
-const copyrightelem = document.createElement('meta');
-copyrightelem.setAttribute('name', 'copyright');
-copyrightelem.setAttribute('content', 'Copyright &COPY; 2024 #8Seven')
+// const copyrightelem = document.createElement('meta');
+// copyrightelem.setAttribute('name', 'copyright');
+// copyrightelem.setAttribute('content', 'Copyright &COPY; 2024 #8Seven')
 // document.head.appendChild(copyrightelem);
 
 export default main;
