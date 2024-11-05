094aeb598cc45698b2415c93471603b309d15cbe
#EightSeven <hasheightseven@gmail.com>
2024-11-05 15:35:46 +0530

    changes

--- src/container.js
+++ src/container.js
@@ -1,6 +1,9 @@
 const container = document.createElement("div");
 container.style.width = "100%";
 container.style.height = "100%";
+container.style.display = 'flex';
+container.style.alignItems = 'center';
+container.style.justifyContent = 'center';
 container.style.backgroundColor = "transparent";
 
 
