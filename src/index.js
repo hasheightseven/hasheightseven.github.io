6bc56f03bf98d849d3ef85760da0a365672c1ec6
#EightSeven <hasheightseven@gmail.com>
2024-11-07 16:53:36 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-import fonts from './fonts.js';
-import defaultstyles from './styles.js';
-if (defaultstyles.hasChildNodes) {
-    console.log(`styles defaults has child nodes`);
-    console.log(defaultstyles.childNodes);
-}
 import main from './main.js';
-import container from "./container.js";
-main.appendChild(container);
-import { loginform, signupform, contactform } from "./forms.js";
-container.appendChild(loginform);
-//import popeye from './popeye.js';
