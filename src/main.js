0df004f10a234de27c1b63196dbbeb147d83505d
#EightSeven <hasheightseven@gmail.com>
2024-11-08 10:19:49 +0530

    changes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 // src/index.js
-import React from 'react';
+import React from '/react';
 import ReactDOM from 'react-dom/client'; // Use 'react-dom/client' in React 18+
 
 import App from './App.js'; // Import your main App component
