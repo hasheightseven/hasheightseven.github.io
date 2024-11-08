c37b48f9dfa3a85f3196844b0e9665ad7d6549e9
#EightSeven <hasheightseven@gmail.com>
2024-11-08 08:39:55 +0530

    react intro

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Use 'react-dom/client' in React 18+
+
+import App from './App.js'; // Import your main App component
+
 // import title and tab title query element
 import { nota, tabtitle } from './titles.js';
 tabtitle.textContent = nota;
@@ -8,9 +14,16 @@ document.head.appendChild(defaultstyles);
 import favicon from './favicon.js';
 document.head.appendChild(favicon);
 // import main content
-// import main from './main.js';
-// document.body.appendChild(main);
-// import greeting
-import greeting, { something } from './greeting.js';
-// something();
-document.body.appendChild(greeting);
+import main from './main.js';
+main.setAttribute('id', 'hesmain')
+document.body.appendChild(main);
+
+// The root element where your React app will be injected
+const root = ReactDOM.createRoot(document.getElementById('hesmain'));
+
+// Render your App component inside the root element
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
