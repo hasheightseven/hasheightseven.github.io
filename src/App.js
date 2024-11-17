75e70ac23b696b685290afe6f67ce610c9c9e2cc
#EightSeven <hasheightseven@gmail.com>
2024-11-17 11:25:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Popeye from './Popeye.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
 	  <img src={logo} className="App-logo" alt="logo" />
+      <Popeye />
       </header>
     </div>
   );
