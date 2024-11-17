fea14d53e7d209b1418db827759800078fccfb75
#EightSeven <hasheightseven@gmail.com>
2024-11-17 11:06:27 +0530

    Update Popeye.jsx

---
+++ src/Popeye.jsx
@@ -0,0 +1,9 @@
+import './popeye.svg'
+
+console.log('ello from popeye.js');
+
+function Popeye() {
+    return (
+        <div class="Popeye-div" style=`background=${'popeye.svg'};`></div>
+    );
+}
