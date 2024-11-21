5af948c3a80e047554b8fe83a5e19312dfcf9e1e
#EightSeven <hasheightseven@gmail.com>
2024-11-21 19:07:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        //<img src={logo} className="App-logo" alt="logo" /> -->
       <Popeye />
       </header>
     </div>
   );
 }
-
+//<img src={logo} className="App-logo" alt="logo" /> -->
 export default App;
