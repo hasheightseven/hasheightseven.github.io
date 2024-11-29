e5911bea3b68c07b0110acf73183ab6496151a4e
#EightSeven <hasheightseven@gmail.com>
2024-11-29 14:29:59 +0530

    Update Popeye.js

--- src/Popeye.js
+++ src/Popeye.js
@@ -4,6 +4,8 @@ function Popeye() {
     return (
         <div class="Popeye-div">
             <img src={popeye} alt="popeye"/>
+            <input id="popeye-name-input" />
+            <label>name ?</label>
         </div>
     );
 }
