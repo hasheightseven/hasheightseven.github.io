121c843ff84f0554b2e8b7fc692e4fd0659ede40
#EightSeven <hasheightseven@gmail.com>
2024-11-29 16:31:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Popeye from './Popeye.js';
+import HesHomeHeader from './HesHomeHeader.js';
 
 function App() {
   return (
     <div className="App">
-        <header className="App-header">
-          <Popeye />
-        </header>
+        //<header className="App-header">
+          //<Popeye />
+        //</header>
+        <HesHomeHeader />
     </div>
   );
 }
