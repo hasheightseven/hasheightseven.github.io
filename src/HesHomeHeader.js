552d1b3f3a0e899514c4fe53339dc55efd08b3e1
#EightSeven <hasheightseven@gmail.com>
2024-11-29 16:28:56 +0530

    Update HesHomeHeader.js

--- src/HesHomeHeader.js
+++ src/HesHomeHeader.js
@@ -1,1 +1,12 @@
-// ello mastaru
+function HesHomeHeader() {
+    return (
+        <img src='#' alt='#EightSeven' />
+        <div id='hes-menu'>
+            <ul id='hes-menu-items'>
+                <li>home</li>
+            </ul>
+        </div>
+    );
+}
+
+export default HesHomeHeader
