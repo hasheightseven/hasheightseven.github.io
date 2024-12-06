341ce02bf0f2b59db06e181c3dc630ecc9ffc431
#EightSeven <hasheightseven@gmail.com>
2024-12-06 11:54:57 +0530

    Update HesHomeHeader.js

--- src/HesHomeHeader.js
+++ src/HesHomeHeader.js
@@ -1,13 +1,13 @@
 function HesHomeHeader() {
     return (
-        <>
+        <div className="HesHomeHeader">
         <img src='#' alt='#EightSeven' />
         <div id='hes-menu'>
             <ul id='hes-menu-items'>
                 <li><a href="/">home</a></li>
             </ul>
         </div>
-        </>
+        </div>
     );
 }
 
