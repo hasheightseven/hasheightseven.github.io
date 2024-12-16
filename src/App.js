e8bbf59f408bbce21d70f1c3f79911477518cd3c
#EightSeven <hasheightseven@gmail.com>
2024-12-16 11:07:38 +0530

    Update SApp.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
-//import Popeye from './Popeye.js';
-import HesHomeHeader from './HesHomeHeader.js';
 
 function App() {
   return (
     <div className="App">
-        <HesHomeHeader />
+      <canvas className="App-Canvas"></canvas>
+      <script>
+        let appcanvas = document.getElementByID("App-Canvas");
+        var ctx = appcanvas.getContext("2d");
+        ctx.moveTo(0,0);
+        ctx.lineTo(200,0);
+        ctx.moveTo(0,1);
+        ctx.lineTo(200,0);
+        ctx.moveTo(0,2);
+        ctx.lineTo(200,0);
+        ctx.moveTo(0,3);
+        ctx.lineTo(200,0);
+        ctx.stroke();
+      </script>
     </div>
   );
 }
-/* <header className="App-header">
-          <Popeye />
-        </header>
-        */
 export default App;
