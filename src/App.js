264cbdff1f907a815057e4d630ed492cd2ae920d
#EightSeven <hasheightseven@gmail.com>
2024-12-17 21:29:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,20 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <canvas className="App-Canvas"></canvas>
-      <script>
-        let appcanvas = document.getElementByID("App-Canvas");
-        var ctx = appcanvas.getContext("2d");
-        ctx.moveTo(0,0);
-        ctx.lineTo(200,0);
-        ctx.moveTo(0,1);
-        ctx.lineTo(200,0);
-        ctx.moveTo(0,2);
-        ctx.lineTo(200,0);
-        ctx.moveTo(0,3);
-        ctx.lineTo(200,0);
-        ctx.stroke();
-      </script>
+      <input className="username-neon-monkey" />
     </div>
   );
 }
