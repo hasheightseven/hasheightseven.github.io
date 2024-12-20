cd07be6c20739042c266a7b61b6d9d2c08403a9b
#EightSeven <hasheightseven@gmail.com>
2024-12-20 17:23:49 +0530

    Update HesHomeHeader.js

--- src/HesHomeHeader.js
+++ src/HesHomeHeader.js
@@ -1,7 +1,9 @@
 function HesHomeHeader() {
     return (
         <div className="HesHomeHeader">
-            <svg xmlns="https://www.w3.org/2000/svg/" viewBox='0 0 30 30'></svg>
+            <svg xmlns="https://www.w3.org/2000/svg/" viewBox='0 0 30 30'>
+                <path d='M10 10 C26 69' fill='#00192826' />
+            </svg>
         </div>
     );
 }
