7526c13a9f008d1dd153a06099479992b99120cf
#EightSeven <hasheightseven@gmail.com>
2024-12-20 18:05:13 +0530

    Update HesHomeHeader.js

--- src/HesHomeHeader.js
+++ src/HesHomeHeader.js
@@ -2,8 +2,8 @@ function HesHomeHeader() {
     return (
         <div className="HesHomeHeader">
             <svg xmlns="https://www.w3.org/2000/svg/" width='200' height='200'>
-                <path d='M50 80 C 65 10 90 10 80 10' stroke='#001928' fill='#001928' />
-                <path d="M10 80 C 25 10 50 10 75 80" stroke="#001928" fill="#001928" />
+                <path d='M26 169 C 15 87 26 26 87 169 Z' stroke='#001928' fill='#001928' />
+                <path d='M50 169 C 36 87 50 26 169 169 Z' stroke='#001928' fill='#001928' />
             </svg>
         </div>
     );
