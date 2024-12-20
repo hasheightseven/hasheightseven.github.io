cbd438ccc2879ae18a794ec68a107ce05a754cea
#EightSeven <hasheightseven@gmail.com>
2024-12-20 19:28:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <svg xmlns="https://www.w3.org/2000/svg/" width='200' height='200'>
                 <path d='M26 169 C 15 87 26 26 87 169 Z' stroke='#001928' fill='#001928' />
                 <path d='M50 169 C 36 87 50 26 169 169 Z' stroke='#001928' fill='#001928' />
-        </svg>
+                <text fill='#001928'>neon monkey</text>
+            </svg>
         <h1 className="HesHomeHeaderHeading">neon monkey</h1>
         </svg>
     </div>
