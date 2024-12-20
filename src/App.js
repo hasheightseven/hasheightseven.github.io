849fc0c8ffec2223f6ae6cdf6f2257a4737012da
#EightSeven <hasheightseven@gmail.com>
2024-12-20 19:26:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+        <div className='HesHomeHeader'>
       <svg xmlns="https://www.w3.org/2000/svg/" width='200' height='200'>
                 <path d='M26 169 C 15 87 26 26 87 169 Z' stroke='#001928' fill='#001928' />
                 <path d='M50 169 C 36 87 50 26 169 169 Z' stroke='#001928' fill='#001928' />
         </svg>
         <h1 className="HesHomeHeaderHeading">neon monkey</h1>
+        </svg>
     </div>
   );
 }
