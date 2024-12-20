97f87b9b866691c74275feb57f6c65b1b215ce62
#EightSeven <hasheightseven@gmail.com>
2024-12-20 19:20:41 +0530

    Update HesHomeHeader.js

--- src/HesHomeHeader.js
+++ src/HesHomeHeader.js
@@ -5,6 +5,7 @@ function HesHomeHeader() {
                 <path d='M26 169 C 15 87 26 26 87 169 Z' stroke='#001928' fill='#001928' />
                 <path d='M50 169 C 36 87 50 26 169 169 Z' stroke='#001928' fill='#001928' />
             </svg>
+            <h1 className="HesHomeHeaderHeading">neon monkey</h1>
         </div>
     );
 }
