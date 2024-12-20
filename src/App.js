29fe8cef9ffee0ba392962322c97afd188ff71db
#EightSeven <hasheightseven@gmail.com>
2024-12-20 19:35:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,16 @@ function App() {
                 <path d='M50 169 C 36 87 50 26 169 169 Z' stroke='#001928' fill='#001928' />
                 <text fill='#001928'>neon monkey</text>
             </svg>
+            <svg xmlns="http://www.w3.org/2000/svg">
+  <path d='M25 8 L50 25 L75 50 Z' fill='#becafe' />
+    <path d='M50 8 L100 25 L100 50 Z' fill='#becafe' />
+      <path d='M75 8 L150 25 L125 50 Z' fill='#becafe' />
+        <path d='M100 8 L200 25 L150 50 Z' fill='#becafe' />
+          <path d='M125 8 L250 25 L175 50 Z' fill='#001928' />
+            <path d='M150 8 L300 25 L200 50 Z' fill='#001928' />
+              <path d='M175 8 L350 25 L225 50 Z' fill='#001928' />
+                <path d='M200 8 L400 25 L250 50 Z' fill='#001928' />
+</svg>
             <h1 className="HesHomeHeaderHeading">neon monkey</h1>
         </div>
     </div>
