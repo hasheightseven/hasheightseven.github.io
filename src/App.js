b3a6e7b7c49fa865269f7a064782a7ce31182341
#EightSeven <hasheightseven@gmail.com>
2024-12-21 19:10:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,8 @@ function App() {
             <img src='./logo.svg' alt='logo-nm' />
             <img src='./logo.svg' alt='logo-nm' />
         </div>
-          <h2 className='greeting'>wanna join the crowd?<br/>Let's join popeye's <a className='.App-Link'>cafe</a></h2>
+          <h2 className='greeting'>wanna join the crowd?<br/>
+    Lets join popeyes <a className='.App-Link'>cafe</a>.</h2>
     </div>
   );
 }
