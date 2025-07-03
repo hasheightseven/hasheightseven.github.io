cfdaf063e9e00a54ee2b4803871ed1d14cc8acac
#EightSeven <hasheightseven@gmail.com>
2025-07-03 21:39:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
         <div className='AppHeader'>
-            <img src='./another-logo.svg' alt='logo-nm' />
+            <img src='./another-logo.svg' alt='logo-nm' class='App-logo'/>
             <h1 className="HesHomeHeaderHeading">neon monkey</h1>
         </div>
     </div>
